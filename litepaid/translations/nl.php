f09db473fba46c100ad3c81f7b8d7dac4b5ada05
Albert Peschar <albert@peschar.net>
2014-11-25 10:52:34 +0100

    add Dutch translations

---
+++ litepaid/translations/nl.php
@@ -0,0 +1,22 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{litepaid}prestashop>litepaid_a1e91797720a3e8b975350182cbbb019'] = 'LitePaid';
+$_MODULE['<{litepaid}prestashop>litepaid_e55fa8aa75f93e484cdaf1576da04640'] = 'Gemakkelijk en snel digitale valuta accepteren.';
+$_MODULE['<{litepaid}prestashop>litepaid_c888438d14855d7d96a2724ee9c306bd'] = 'Instellingen bijgewerkt';
+$_MODULE['<{litepaid}prestashop>admin_form_622b569317ce1dc13df1a8f821245217'] = 'LitePaid instellingen';
+$_MODULE['<{litepaid}prestashop>admin_form_656a6828d7ef1bb791e42087c4b5ee6e'] = 'API key';
+$_MODULE['<{litepaid}prestashop>admin_form_352d8151277635a0d7146bf6cfd1a9f9'] = 'Status bij succesvolle betaling';
+$_MODULE['<{litepaid}prestashop>admin_form_0a499f87b58637a3602b26ed9b1c1fb9'] = 'Status bij mislukte betaling';
+$_MODULE['<{litepaid}prestashop>admin_form_4245499695408b974322be6f01b0d17a'] = 'Testmodus';
+$_MODULE['<{litepaid}prestashop>admin_form_282d5ae2bc2345100fa1d6f8422cdcda'] = 'Ja, schakel testmode in en verwerk betalingen niet';
+$_MODULE['<{litepaid}prestashop>admin_form_48394705e98fb357da6f0df2bf4910eb'] = 'Nee, schakel testmode uit';
+$_MODULE['<{litepaid}prestashop>admin_form_daa6b483e9f6ca081ec7e0b4a352f9e9'] = 'Wijzigingen opslaan';
+$_MODULE['<{litepaid}prestashop>payment_dc0e194e6e48cb95b48acd47402b8ed7'] = 'Betaal met LitePaid';
+$_MODULE['<{litepaid}prestashop>payment_7d7ac73b05977a309c4cc616cf94f573'] = 'Betaal met digitale valuta via LitePaid';
+$_MODULE['<{litepaid}prestashop>payment_return_2e2117b7c81aa9ea6931641ea2c6499f'] = 'Uw bestelling op';
+$_MODULE['<{litepaid}prestashop>payment_return_75fbf512d744977d62599cc3f0ae2bb4'] = 'is voltooid.';
+$_MODULE['<{litepaid}prestashop>payment_return_cfa0f5d192620d230ccbf3664d9d0610'] = 'Het toaalbedrag van deze order is';
+$_MODULE['<{litepaid}prestashop>payment_return_8de637e24570c1edb0357826a2ad5aea'] = 'Er was een probleem met uw betaling. Als dit een fout is, neem dan contact op met onze';
+$_MODULE['<{litepaid}prestashop>payment_return_64430ad2835be8ad60c59e7d44e4b0b1'] = 'klantenservice';
